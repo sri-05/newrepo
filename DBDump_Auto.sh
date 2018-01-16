0323bd3571fcdba560eb7e00de1d81381ac53297
sri-05 <35294688+sri-05@users.noreply.github.com>
2018-01-16 20:04:21 +0530

    Update DBDump_Auto.sh

--- DBDump_Auto.sh
+++ DBDump_Auto.sh
@@ -2,3 +2,7 @@
 
 mysqldump -uroot -pPassword1 dbname1 > dbname1`date +"%F:%H:%M:%S"`.sql 2> /dev/null
 sleep 10
+mysqldump -uroot -pPassword2 dbname2 > dbname2`date +"%F:%H:%M:%S"`.sql 2> /dev/null
+cd /root/Sri/DBBackup/
+sleep 20
+find . -mtime +2 -name '*.sql' -exec rm -rf {} \;

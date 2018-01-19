de9f992eaf3ce2dbcda1691c9cec252e3e2acb1e
sri <sridhar.sri.may5@gmail.com>
2018-01-19 08:12:22 +0000

    Script to monitor service

---
+++ Service_monitor.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+IPADDR=192.X.X.10
+
+`rm -rf 503.txt`
+
+tail -n5 /etc/httpd/logs/access_log | awk '{ if($9 == 503) { print $1 } }' > 503.txt
+
+count=`cat 503.txt | wc -l`
+
+if [ "$count" -ge "4" ]
+
+then
+
+IP=`cat 503.txt |tail -n1 | awk '{print $1}'`
+
+if [ "$IP" == "$IPADDR" ]
+ then
+
+
+echo "Found 503!!!!!!! Some internal Error Occured"
+
+fi
+
+fi

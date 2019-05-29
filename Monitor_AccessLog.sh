3e74e9fde7eb7beeea99d80430900dae40468385
sri-05 <35294688+sri-05@users.noreply.github.com>
2019-05-29 15:49:13 +0530

    Update Monitor_AccessLog.sh

--- Monitor_AccessLog.sh
+++ Monitor_AccessLog.sh
@@ -3,15 +3,13 @@
 
 IPADDR=192.X.X.10
 
-`rnm -rf 503.txt`
+`rm -rf 503.txt`
 
 tail -n5 /etc/httpd/logs/access_log | awk '{ if($9 == 503) { print $1 } }' > 503.txt
 
 count=`cat 503.txt | wc -l`
 
 if [ "$count" -ge "4" ]
-
-then
 
 IP=`cat 503.txt |tail -n1 | awk '{print $1}'`
 
